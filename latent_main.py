b67e160de6bab732d29824e6c46c060f424fa988
Pranjal Gupta <pranjalgupta2199@gmail.com>
2019-11-24 13:31:02 +0530

    misc: Adds entry point for latent factor

--- latent_main.py
+++ latent_main.py
@@ -3,14 +3,9 @@ from latent_factor.model import LatentFactor
 import time
 
 if __name__ == "__main__":
-    # lf.preprocess()
     model = LatentFactor(k=50, epoch=70, beta=0.1, alpha=0.01)
     model.train()
     print("Validation Dataset {}".format(
         model.get_mean_abs_error(model.validation_dataset)))
     print("Validation Dataset {}\n\n".format(
         model.get_rms_error(model.validation_dataset)))
-    start = time.time()
-    print("Time taken to predict {}\n\n".format(model.predict(40, 65)))
-    end = time.time()
-    print(end - start)
